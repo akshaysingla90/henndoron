249f77a91e3e4cc831bdfbd162c9c157cee1a7d5
Rajan <rajan@chicmic.co.in>
2020-05-19 10:31:23 +0530

    --Change type

--- app/models/helen/roomModel.js
+++ app/models/helen/roomModel.js
@@ -6,8 +6,8 @@ const Schema = MONGOOSE.Schema;
  ************* Room Model or collection ***********
  **************************************************/
 const roomSchema = new Schema({
-    users: [{ userId: { type: String } }],
-    createdBy: { type: String, required: true },
+    users: [{ userId: { type: Schema.Types.ObjectId } }],
+    createdBy: { type: Schema.Types.ObjectId, required: true },
     capacity: { type: Number, default: 1 },
     isDeleted: { type: Boolean, default: false }
 });
