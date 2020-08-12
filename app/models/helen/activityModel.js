931a0e9ea5d9988bb0f9e8cbc3c41180daa53003
Akshay Singla <akshay.singla@chicmic.co.in>
2020-08-12 18:29:31 +0530

    -- pre-hook to handle unique key errors

--- app/models/helen/activityModel.js
+++ app/models/helen/activityModel.js
@@ -2,7 +2,9 @@
 /************* Modules ***********/
 const MONGOOSE = require('mongoose');
 const Schema = MONGOOSE.Schema;
-const { ACTIVITY_TYPE } = require('../../utils/constants')
+const { ACTIVITY_TYPE, ERROR_TYPES } = require('../../utils/constants')
+const HELPERS = require("../../helpers");
+
 /**************************************************
  ************* User Model or collection ***********
  **************************************************/
@@ -23,5 +25,14 @@ const activitySchema = new Schema({
 });
 
 activitySchema.set('timestamps', true);
+
+// pre-hook to handle unique key errors
+activitySchema.post('save', async (error, doc, next) => {
+    if (error.name === 'MongoError' && error.code === 11000)
+        throw HELPERS.responseHelper.createErrorResponse(`${Object.keys(error.keyPattern)[0]} must be unique`, ERROR_TYPES.BAD_REQUEST);
+    else
+        next(error);
+    next();
+});
 
 module.exports = MONGOOSE.model('activity', activitySchema);
