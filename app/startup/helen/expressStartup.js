82b3b774420c2a2d3b8f0dc8c12ed75aee021c57
Akshay Singla <akshay.singla@chicmic.co.in>
2020-09-10 11:43:46 +0530

    - added cors middleware

--- app/startup/helen/expressStartup.js
+++ app/startup/helen/expressStartup.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const express = require('express');
+const cors = require('cors');
 
 const routes = require('../../routes');
 const routeUtils = require('../../utils/routeUtils');
@@ -32,13 +33,15 @@ module.exports = async function (app) {
     /********************************
     ***** For handling CORS Error ***
     *********************************/
-    app.all('/*', (request, response, next) => {
-        response.header('Access-Control-Allow-Origin', '*');
-        response.header('Access-Control-Allow-Headers', 'Content-Type, api_key, Authorization, x-requested-with, Total-Count, Total-Pages, Error-Message');
-        response.header('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT, OPTIONS');
-        response.header('Access-Control-Max-Age', 1800);
-        next();
-    });
+    // app.all('/*', (request, response, next) => {
+    //     response.header('Access-Control-Allow-Origin', '*');
+    //     response.header('Access-Control-Allow-Headers', 'Content-Type, api_key, Authorization, x-requested-with, Total-Count, Total-Pages, Error-Message');
+    //     response.header('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT, OPTIONS');
+    //     response.header('Access-Control-Max-Age', 1800);
+    //     next();
+    // });
+    // Enable cors
+    app.use(cors());
 
     // serve static folder.
     // app.use('/public', express.static('public'));
