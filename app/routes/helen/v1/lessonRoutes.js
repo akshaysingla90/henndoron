4147d965e2f91d9859e6f4eea1cc78a4f2b79fe8
Akshay Singla <akshay.singla@chicmic.co.in>
2020-09-24 15:00:47 +0530

    -Change courseId joi schema to string from Number

--- app/routes/helen/v1/lessonRoutes.js
+++ app/routes/helen/v1/lessonRoutes.js
@@ -54,7 +54,8 @@ let routes = [
             activityId: Joi.string().required().description('Module Id.'),
             activityName: Joi.string().required().description('Module name'),
             allocatedTime: Joi.number().required().description('Module time')
-          }).required()).description('Activity Ids in order'),        courseId: Joi.string().required().description('Course Id'),
+          }).required()).description('Activity Ids in order'),
+        courseId: Joi.string().required().description('Course Id'),
         status: Joi.number().valid(LESSON_STATUS.DRAFT, LESSON_STATUS.PUBLISHED).description('3 => DRAFT,4 => PUBLISHED'),
       },
       group: 'Lesson',
