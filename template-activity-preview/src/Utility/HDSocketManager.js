7cc7d4ba009f9ccc0396fd99e2e793351e0833ba
Divyan Soni <divyan.soni@chicmic.co.in>
2020-11-26 18:51:26 +0530

    - update core lesson template files.

--- template-activity-preview/src/Utility/HDSocketManager.js
+++ template-activity-preview/src/Utility/HDSocketManager.js
@@ -6,55 +6,54 @@
  */
 
 const SocketEvents = {
-    Connect                         :   "connect",
-    Reconnect                       :   "reconnect",
-    Reconnecting                    :   "reconnecting",
-    Disconnect                      :   'disconnect',
-    Error                           :   "error",
-    Message                         :   "message",
-    EventTest                       :   "Test"
+  Connect: "connect",
+  Reconnect: "reconnect",
+  Reconnecting: "reconnecting",
+  Disconnect: "disconnect",
+  Error: "error",
+  Message: "message",
+  EventTest: "Test",
 };
 
 const SocketManager = {
-    socket : null,
-    socketConfig : {},
-    connect : function () {
-        cc.log("URl -"+ HDAppManager.getSocketURL());
-        this.socket = io(HDAppManager.getSocketURL(), this.socketConfig);
-        this.socket.on(SocketEvents.Connect, function(){
-            console.log('======socket connected sucessfully======');
-        });
-        this.socket.on(SocketEvents.Error, function(error){
-            console.log('======socket connection error====== :'+error.toString());
-        });
-        this.socket.on(SocketEvents.Disconnect, function(){
-            console.log('======socket disconnected sucessfully======');
-        });
-        this.socket.on(SocketEvents.Reconnect, function(){
-            console.log('======socket reconnected sucessfully======');
-        });
-        this.socket.on(SocketEvents.Reconnecting, function(msg){
-            console.log('======socket Reconnecting sucessfully====== :'+msg.toString());
-        });
-        this.socket.on(SocketEvents.Message, function(data){
-            console.log('======socket message receved====== :'+data.toString());
-        });
-    },
+  socket: null,
+  socketConfig: {},
+  connect: function () {
+    cc.log("URl -" + HDAppManager.getSocketURL());
+    this.socket = io(HDAppManager.getSocketURL(), this.socketConfig);
+    this.socket.on(SocketEvents.Connect, function () {
+      console.log("====== socket connected sucessfully ======");
+    });
+    this.socket.on(SocketEvents.Error, function (error) {
+      console.log("====== socket connection error ====== :" + error.toString());
+    });
+    this.socket.on(SocketEvents.Disconnect, function () {
+      console.log("====== socket disconnected sucessfully======");
+    });
+    this.socket.on(SocketEvents.Reconnect, function () {
+      console.log("====== socket reconnected sucessfully======");
+    });
+    this.socket.on(SocketEvents.Reconnecting, function (msg) {
+      console.log("====== socket Reconnecting successfully====== :" + msg.toString());
+    });
+    this.socket.on(SocketEvents.Message, function (data) {
+      console.log("====== socket message received ====== :" + data.toString());
+    });
+  },
 
-    isSocketConnected : function(){
-        return this.socket.connected;
-    },
+  isSocketConnected: function () {
+    return this.socket.connected;
+  },
 
-    emitCutomEvent : function (eventName, eventData, fn) {
-        this.socket.emit(eventName, eventData, fn);
-    },
+  emitCutomEvent: function (eventName, eventData, fn) {
+    this.socket.emit(eventName, eventData, fn);
+  },
 
-    subscribeEvent : function (eventName, fn) {
-        this.socket.on(eventName,fn);
-    },
+  subscribeEvent: function (eventName, fn) {
+    this.socket.on(eventName, fn);
+  },
 
-    removeSubscribedEvent : function (eventName, fn) {
-     //   this.socket.removeListener(eventName,fn);
-    }
-
+  removeSubscribedEvent: function (eventName, fn) {
+    //   this.socket.removeListener(eventName,fn);
+  },
 };
